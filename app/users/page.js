bb43cda33777476daf9825cd8e17790099c24387
Zach Raab <zpraab@hotmail.com>
2025-01-17 14:51:19 -0700

    Update user page to fetch data from users api

--- app/users/page.js
+++ app/users/page.js
@@ -1,15 +1,47 @@
-export default async function UsersPage() {
-	const response = await fetch("https://jsonplaceholder.typicode.com/users");
-	const users = await response.json();
+'use client';
+
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+export default function UsersPage() {
+	const [users, setUsers] = useState([]);
+	const [loading, setLoading] = useState(true);
+  
+	useEffect(() => {
+		// Fetch the users from the API route
+		async function fetchUsers() {
+			try {
+				const response = await fetch('/api/users');
+				if (!response.ok) {
+					throw new Error('Failed to fetch users');
+				}
+				const data = await response.json();
+				setUsers(data);
+				setLoading(false);
+			} catch (error) {
+				console.error('Error fetching users:', error);
+				setLoading(false);
+			}
+		}
+	
+		fetchUsers();
+	}, []);
+	
+	if (loading) {
+		return <div>Loading...</div>;
+	}
+
 	return (
-			<>
-				<h1>UsersPage</h1>
-				{users.map((user, index) => (
-					<div key={index}>
-						<p>{user.name}</p>
-						<h3>{user.email}</h3>
-					</div>
+		<div className='text-center'>
+			<h1>Users List</h1>
+			<ul>
+				{users.map((user) => (
+					<li key={user.user_id}>
+						{user.first_name} {user.last_name} - {user.email}
+					</li>
 				))}
-			</>
+			</ul>
+			<Link href='/api/users' className='bg-green-300 hover:underline'>User API</Link>
+		</div>
 	);
 }
